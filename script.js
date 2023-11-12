4994fc443df8b9cc4702bb4206e2b1be66881853
Robert Solorzano <robertsolorzano510@gmail.com>
2023-11-12 12:36:38 -0800

    adjusted about me text

---
+++ script.js
@@ -0,0 +1,9 @@
+//created function for button, directing to projects but later down the road will direct possible to something else
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+    window,location.href = '#projects';
+console.log('Clicked')
+})
+
