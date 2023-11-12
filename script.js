32dcefa28130ec920e61a4a64a0b1ce3ec705c5b
Robert Solorzano <robertsolorzano510@gmail.com>
2023-11-12 12:38:14 -0800

    added script for button logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//created function for button, directing to projects but later down the road will direct possible to something else
+//created function for button, directing to projects but later down the road will direct possible to something else.
 
 const button = document.querySelector('button');
 
