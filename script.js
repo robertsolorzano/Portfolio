08bd4b51995fb9e9ebb62d84d22dc3cae4864f3e
Robert Solorzano <robertsolorzano510@gmail.com>
2023-11-17 11:23:50 -0800

    adjusted sizing for smaller screens

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//created function for button, directing to projects but later down the road will direct possible to something else.
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', function() {
-    window,location.href = '#projects';
+    window.location.href = '#projects';
 console.log('Clicked')
 })
 
